352879203b0b13f8082ec050106451d390f46971
Ez-zouek Hamza <ezzouekhamza2411@gmail.com>
2026-03-03 19:35:15 +0000

    feat: Add utility script to log specific React icons from `si` and `tb` modules.

---
+++ find_icons.js
@@ -0,0 +1,7 @@
+
+import * as si from 'react-icons/si';
+import * as tb from 'react-icons/tb';
+
+console.log('SI C:', Object.keys(si).filter(k => k.startsWith('SiC')));
+console.log('TB C:', Object.keys(tb).filter(k => k.startsWith('TbC')));
+
